2c839b537364c936edb5bf8af95d48928031a68b
Mats de Beer <the-goblin@scrying.orb>
2025-03-10 14:39:34 +0100

    pluh

--- inc/atohallan.h
+++ inc/atohallan.h
@@ -5,21 +5,25 @@
 typedef struct s_chessboard t_board;
 struct s_chessboard
 {
-	uint64_t	whiteKings;
-	uint64_t	blackKings;
-	uint64_t	whiteQueens;
-	uint64_t	blackQueens;
-	uint64_t	whiteBishops;
-	uint64_t	blackBishops;
-	uint64_t	whiteKnights;
-	uint64_t	blackKnights;
-	uint64_t	whiteRooks;
-	uint64_t	blackRooks;
-	uint64_t	whitePawns;
-	uint64_t	blackPawns;
+	uint64_t	wK;
+	uint64_t	bK;
+	uint64_t	wQ;
+	uint64_t	bQ;
+	uint64_t	wB;
+	uint64_t	bB;
+	uint64_t	wN;
+	uint64_t	bN;
+	uint64_t	wR;
+	uint64_t	bR;
+	uint64_t	wP;
+	uint64_t	bP;
 };
 
 #define EMPTY ((uint64_t)0)
 #define FULL ((uint64_t)-1)
+#define WHITE(x) ((x).wK | (x).wQ | (x).wB | (x).wN | (x).wR | (x).wP)
+#define BLACK(x) ((x).bK | (x).bQ | (x).bB | (x).bN | (x).bR | (x).bP)
+#define ALL(x) (WHITE(x) | BLACK(x))
+#define OPEN(x) (~(ALL(x)))
 
 #endif
--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 
 // Do you wanna build a chessbot??????
 
+#include <stdint.h>
 #include <stdio.h>
 #include <atohallan.h>
 
@@ -11,6 +12,13 @@ int argc,
 char **argv
 )
 {
+	uint64_t	numbie;
+	uint64_t	count;
+
+	printf("please enter a numbie\n");
+	scanf("%zu", &numbie);
+	asm("popcnt %1,%0" : "=r"(count) : "rm"(numbie) : "cc");
+	printf("%zu popcnt %zu\n", numbie, count);
 	printf("leave me alone anna!\n");
 	return (1);
 }
